9689bf003e5f5d137570b6ca2551e2c264d0243d
Manya Sharma <manya.sharma395@gmail.com>
2025-06-18 17:27:27 +0530

    Update script.js by adding pen up and pen down features

--- script.js
+++ script.js
@@ -3,10 +3,11 @@ const newPageBtn = document.querySelector(".newPageBtn");
 const resetPageBtn = document.querySelector(".resetPageBtn");
 
 let currentGridSize = 16;
+let penDown = false; // Variable to track if the mouse button is pressed
 
 const createGrid = (size) => {
     container.innerHTML = "";
-    const squareSize = 592 / size;
+    const squareSize = 592 / size;      // Calculate square size based on container width (592px)
 
     for(let i = 0; i < size * size; i++){
         const square = document.createElement("div");
@@ -18,11 +19,11 @@ const createGrid = (size) => {
         square.style.backgroundColor = "rgb(231, 27, 231)";
         });
 
-        container.appendChild(square);
+        container.appendChild(square); // Append the square to the container
     }
 }
 
-createGrid(16);
+createGrid(16); // Initial grid size
 
 newPageBtn.addEventListener("click", () => {
     let noOfSquares = parseInt(prompt("Enter the number of squares per side:"));
@@ -38,6 +39,21 @@ resetPageBtn.addEventListener("click", () => {
     createGrid(currentGridSize);
 });
 
+container.addEventListener("mousedown", (e) => { 
+    if(e.button === 0) {                              // Check if left mouse button is pressed
+        penDown = true;                               
+    }
+});
+
+document.addEventListener("mouseup", () => {
+    penDown = false;
+})
+
+container.addEventListener("mouseover", (e)=>{
+    if(penDown && e.target.classList.contains("square")){
+        e.target.style.backgroundColor = "#f0f0f0";
+    }
+});
 
 
 
